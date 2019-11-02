407baf84a0d87be5f4798a632593f29bf5e203a1
Charly Berthet <charlyberthet@MacBook-Pro-de-Charly.local>
2019-11-02 21:58:03 +0100

    clean (delete all)

--- enculette/data/js/controllers/contentController.js
+++
@@ -1,6 +0,0 @@
-myApp.controller('contentController', function($scope) {
-    $scope.lancer = function(){
-        $scope.deA = Math.floor( (Math.random() * 6) +1) ;
-        $scope.deB = Math.floor( (Math.random() * 6) +1) ;
-    };
-});
